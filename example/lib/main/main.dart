1246b528e1e6f2940a2e69feefc16005a91addf9
Korfas <cihanurtekin@gmail.com>
2021-01-17 12:43:19 +0300

    example projesi içinde, main.dart ve home_page.dart oluşturuldu. pubspec içine firebase_core eklendi. main.dart içinde Fİrebase initialize edildi.
    ana pakette, notification_service_fcm içindeki initialize metodunun içi yorum satırı haline getirildi. Çünkü fxm paketi güncellendiği için "configure" metodu tanınmıyordu. Daha sonra bakılacak.

---
+++ example/lib/main/main.dart
@@ -0,0 +1,28 @@
+import 'package:firebase_core/firebase_core.dart';
+import 'package:flutter/material.dart';
+
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
+  //setupLocator();
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: Sentences.appTitle(),
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: HomePage(title: Sentences.homePageTitle()),
+      routes: router.Router.getRoutes(),
+      localizationsDelegates: [
+        GlobalMaterialLocalizations.delegate,
+        GlobalWidgetsLocalizations.delegate
+      ],
+      supportedLocales: [Locale('en', ''), Locale('tr', '')],
+    );
+  }
+}
